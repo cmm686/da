a12519620181511eb15509118bac42183613ce4f
cmm686 <85597092+cmm686@users.noreply.github.com>
2021-07-26 07:26:13 +0800

    Update jdFruitShareCodes.js

--- zuozheku0301/jd_scripts/jdFruitShareCodes.js
+++ zuozheku0301/jd_scripts/jdFruitShareCodes.js
@@ -12,25 +12,25 @@ let FruitShareCodes = [
 ]
 
 // 从日志获取互助码
-// const logShareCodes = require('./utils/jdShareCodes');
-// if (logShareCodes.FRUITSHARECODES.length > 0 && !process.env.FRUITSHARECODES) {
-//   process.env.FRUITSHARECODES = logShareCodes.FRUITSHARECODES.join('&');
-// }
+ const logShareCodes = require('./utils/jdShareCodes');
+ if (logShareCodes.FRUITSHARECODES.length > 0 && !process.env.FRUITSHARECODES) {
+   process.env.FRUITSHARECODES = logShareCodes.FRUITSHARECODES.join('&');
+ }
 
 // 判断github action里面是否有东东农场互助码
-if (process.env.FRUITSHARECODES) {
-  if (process.env.FRUITSHARECODES.indexOf('&') > -1) {
-    console.log(`您的东东农场互助码选择的是用&隔开\n`)
-    FruitShareCodes = process.env.FRUITSHARECODES.split('&');
-  } else if (process.env.FRUITSHARECODES.indexOf('\n') > -1) {
-    console.log(`您的东东农场互助码选择的是用换行隔开\n`)
-    FruitShareCodes = process.env.FRUITSHARECODES.split('\n');
-  } else {
-    FruitShareCodes = process.env.FRUITSHARECODES.split();
-  }
-} else {
-  console.log(`由于您环境变量(FRUITSHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
-}
+//if (process.env.FRUITSHARECODES) {
+//  if (process.env.FRUITSHARECODES.indexOf('&') > -1) {
+//    console.log(`您的东东农场互助码选择的是用&隔开\n`)
+ //   FruitShareCodes = process.env.FRUITSHARECODES.split('&');
+//  } else if (process.env.FRUITSHARECODES.indexOf('\n') > -1) {
+//    console.log(`您的东东农场互助码选择的是用换行隔开\n`)
+ //   FruitShareCodes = process.env.FRUITSHARECODES.split('\n');
+ // } else {
+ //   FruitShareCodes = process.env.FRUITSHARECODES.split();
+//  }
+//} else {
+//  console.log(`由于您环境变量(FRUITSHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+//}
 for (let i = 0; i < FruitShareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
   exports['FruitShareCode' + index] = FruitShareCodes[i];
